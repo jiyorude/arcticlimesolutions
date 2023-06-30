ca6ecdd3ff3fd12a9938960d0045c0cb2827cca9
Jordy Veenstra <jordy.gaptx@gmail.com>
2023-07-01 01:22:33 +0200

    01/07 01:22
    End of Day Commit, added WhatsApp to headerbar

--- arcticlimesolutions/src/components/Header.js
+++ arcticlimesolutions/src/components/Header.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import "../css/header.min.css";
 import { ReactComponent as Logo } from '../image/svg/arclime.svg';
+import { ReactComponent as WhatsApp } from '../image/svg/whatsapp.svg';
 
 const Header = () => {
   return (
     <>
-      <header>
+      <div className="width">
+        <header>
           <Logo />
-      </header>
+          <ul>
+            <li>
+              <a href="https://api.whatsapp.com/send?phone=31617014448" title="Contact me through WhatsApp">
+                <span>Contact ARCTIC LIME Solutions through WhatsApp.</span>
+                <WhatsApp />
+              </a>
+            </li>
+          </ul>
+        </header>
+      </div>
     </>
   );
 }
