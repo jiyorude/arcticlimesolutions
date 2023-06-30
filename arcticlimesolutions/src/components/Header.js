3ba992bef3f54db876b695d7139f9229cc92bb61
Jordy Veenstra <jordy.gaptx@gmail.com>
2023-06-30 19:17:31 +0200

    30/06 19:17
    Added missing files

---
+++ arcticlimesolutions/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// header scss
+
+const Header = () => {
+  return (
+    <>
+    <div>ARCTIC LIME Solutions</div>
+    <div>LOGO</div>
+    <div>PAYOFF</div>
+    </>
+  )
+}
+
+export default Header
