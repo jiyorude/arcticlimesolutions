6195a166da9dea484244e9bdec77d798c741194d
Jordy Veenstra <jordy.gaptx@gmail.com>
2023-07-02 16:45:52 +0200

    02/07 16:45
    Finished header component, started on footer component, before dinner commit

---
+++ arcticlimesolutions/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+// Footer with copyright that changes upon the year, plus news ticker.
+export default Footer
