50e0221ebdde195da4b8b5b810c03c6aca1d8dab
Jordy Veenstra <jordy.gaptx@gmail.com>
2023-07-03 17:31:30 +0200

    03/07 17:31
    Rough code for footerblock ready

--- arcticlimesolutions/src/components/Footer.js
+++ arcticlimesolutions/src/components/Footer.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import "../css/footer.min.css";
 
 const Footer = () => {
+  // will have to be implemented later in arcinfo.
+  /*const date = new Date();
+  const year = date.getFullYear();*/
   return (
-    <div>Footer</div>
+    <>
+    <section className="footerblock">
+      <div className="newsticker">HERE IS GOING TO BE THE NEWSTICKER</div>
+      <div className="arcinfo">HERE IS GOING TO BE COMPANY INFO, COPYRIGHT, KVK ETC.</div>
+      <div className="privterms">HERE IS GOING TO BE BUTTONS TO PRIVACY AND TERMS OF CONDITIONS</div>
+    </section>
+    </>
   )
 }
 // Footer with copyright that changes upon the year, plus news ticker.
