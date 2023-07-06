4ff9d0e1d6d01f3145f01be4a38f0e67cdaba2db
Jordy Veenstra <jordy.gaptx@gmail.com>
2023-07-06 22:40:07 +0200

    06/07 22:39
    Updated center area of footer

--- arcticlimesolutions/src/components/Footer.js
+++ arcticlimesolutions/src/components/Footer.js
@@ -27,39 +27,53 @@ const Footer = () => {
 
   return (
     <>
-    <section className="footerblock">
-      <div className="newsticker">HERE IS GOING TO BE THE NEWSTICKER</div>
-    
-    {/*Copyright plus company info. KVK number and BTW number HERE */}
-      <div className="arcinfo">
-        <div className="info">
-          <div className="copy">
-          <h3>&copy; ARCTIC LIME Solutions {year}</h3>
+      <section className="footerblock">
+        <div className="newsticker">HERE IS GOING TO BE THE NEWSTICKER</div>
+
+        {/*Copyright plus company info. KVK number and BTW number HERE */}
+        <div className="arcinfo">
+          <div className="info">
+            <div className="copy">
+              <h3>&copy; ARCTIC LIME Solutions {year}</h3>
+            </div>
+             <div className="coc">
+              <h5>KVK/COC: 85701319</h5>
+            </div>
+            <div className="vat">
+            <h5>BTW/VAT: NL004134441B80</h5></div>
           </div>
-          <h5>Kamer van Koophandel / Chamber of Commerce:</h5>
-          <h6>85701319</h6>
-          <h5>BTW Nummer / VAT Number:</h5>
-          <h6>NL004134441B80</h6>
         </div>
-      </div>
-      
-      <div className="privterms">
-        <div className="btncontainer">
-          <ul>
-            <li><h2 style={{color: privHover ? "#D0FF14" : "#FAFAFA"}} 
-            onMouseEnter={handlePrivHover}
-            onMouseLeave={returnPrivHover} title="Learn more about how your data is processed">PRIVACY STATEMENT</h2>
-            <span>Click here to read ARCTIC LIME's Privacy Statement</span></li>
-            <li><h2 style={{color: termHover ? "#D0FF14" : "#FAFAFA"}}
-            onMouseEnter={handleTermHover}
-            onMouseLeave={returnTermHover} title="Click here for the Terms and Conditions">TERMS AND CONDITIONS</h2>
-            <span>Click here to read ARCTIC LIME's general Terms and Conditions</span></li>
-          </ul>
+        <div className="privterms">
+          <div className="btncontainer">
+            <ul>
+              <li>
+                <h2
+                  style={{ color: privHover ? "#D0FF14" : "#FAFAFA" }}
+                  onMouseEnter={handlePrivHover}
+                  onMouseLeave={returnPrivHover}
+                  title="Learn more about how your data is processed"
+                >
+                  PRIVACY STATEMENT
+                </h2>
+                <span>Click here to read ARCTIC LIME's Privacy Statement</span>
+              </li>
+              <li>
+                <h2
+                  style={{ color: termHover ? "#D0FF14" : "#FAFAFA" }}
+                  onMouseEnter={handleTermHover}
+                  onMouseLeave={returnTermHover}
+                  title="Click here for the Terms and Conditions"
+                >
+                  TERMS AND CONDITIONS
+                </h2>
+                <span>Click here to read ARCTIC LIME's general Terms and Conditions</span>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
     </>
-  )
+  );
 }
 // Footer with copyright that changes upon the year, plus news ticker.
 export default Footer
