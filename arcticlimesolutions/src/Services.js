d8d6016b177684aec0ba3e9b0d34240594251f4e
Jordy Veenstra <jordy.gaptx@gmail.com>
2023-07-19 16:30:50 +0200

    19/07 16:30
    Created grid for services page. Grid fully responsive

--- arcticlimesolutions/src/Services.js
+++ arcticlimesolutions/src/Services.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './components/Header'
 import FooterAlt from "./components/FooterAlt";
+import ServicesContent from './components/ServicesContent';
 
 const Services = () => {
   return (
     <>
     <Header />
+    <ServicesContent />
     <FooterAlt selectedID={0} />
     </>
   )
---
+++ arcticlimesolutions/src/components/ServicesContent.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "../css/services.min.css";
+
+const ServicesContent = () => {
+  return (
+    <main className="flowroot">
+    <section className="servicegrid">
+        <div className="servicecontent">Video Editing</div>
+        <div className="servicecontent">Motion Graphics</div>
+        <div className="servicecontent">Audio Post-Production</div>
+        <div className="servicecontent">Animation</div>
+        <div className="servicecontent">Subtitling</div>
+        <div className="servicecontent">Translation</div>
+        <div className="servicecontent">Delivery</div>
+        <div className="servicecontent">DVD/Blu-Ray Authoring</div>
+        <div className="servicecontent">File Duplication</div>
+        <div className="servicecontent">Digitization/Upscaling</div>
+        <div className="servicecontent">UX/UI Design</div>
+        <div className="servicecontent">Web/Software Development</div>
+     </section>
+    </main>
+  )
+}
+
+export default ServicesContent
