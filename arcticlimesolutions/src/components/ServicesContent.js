94b52b103c6b891cb97f30a8318d44f821c469ae
Jordy Veenstra <jordy.gaptx@gmail.com>
2023-07-19 17:46:08 +0200

    19/07 17:42
    Working on content in service grid

--- arcticlimesolutions/src/components/ServicesContent.js
+++ arcticlimesolutions/src/components/ServicesContent.js
@@ -1,25 +1,79 @@
-import React from 'react'
+import React from "react";
 import "../css/services.min.css";
+import Editing from '../image/jpg/services/Editing.jpg';
+import MoGraph from "../image/jpg/services/Mograph.jpg";
+import Audio from "../image/jpg/services/Audio.jpg";
+import Animation from "../image/jpg/services/Animatie.jpg";
+import Subtitling from "../image/jpg/services/Subtitling.jpg";
 
 const ServicesContent = () => {
   return (
     <main className="flowroot">
-    <section className="servicegrid">
-        <div className="servicecontent">Video Editing</div>
-        <div className="servicecontent">Motion Graphics</div>
-        <div className="servicecontent">Audio Post-Production</div>
-        <div className="servicecontent">Animation</div>
-        <div className="servicecontent">Subtitling</div>
-        <div className="servicecontent">Translation</div>
-        <div className="servicecontent">Delivery</div>
-        <div className="servicecontent">DVD/Blu-Ray Authoring</div>
-        <div className="servicecontent">File Duplication</div>
-        <div className="servicecontent">Digitization/Upscaling</div>
-        <div className="servicecontent">UX/UI Design</div>
-        <div className="servicecontent">Web/Software Development</div>
-     </section>
+      <section className="servicegrid">
+        <div
+          className="servicecontent editing"
+          style={{
+            backgroundImage: `url(${Editing})`,
+          }}
+        >
+          <p className="context">Video Editing</p>
+        </div>
+        <div
+          className="servicecontent mograph"
+          style={{
+            backgroundImage: `url(${MoGraph})`,
+          }}
+        >
+          <p className="context">Motion Graphics</p>
+        </div>
+        <div
+          className="servicecontent audio"
+          style={{
+            backgroundImage: `url(${Audio})`,
+          }}
+        >
+          <p className="context">Audio Post</p>
+        </div>
+        <div
+          className="servicecontent animation"
+          style={{
+            backgroundImage: `url(${Animation})`,
+          }}
+        >
+          <p className="context">Animation</p>
+        </div>
+        <div
+          className="servicecontent subs"
+          style={{
+            backgroundImage: `url(${Subtitling})`,
+          }}
+        >
+          <p className="context">Subtitling</p>
+        </div>
+        <div className="servicecontent">
+          <p className="context">Translation</p>
+        </div>
+        <div className="servicecontent">
+          <p className="context">Delivery</p>
+        </div>
+        <div className="servicecontent">
+          <p className="context">DVD/Blu-Ray Authoring</p>
+        </div>
+        <div className="servicecontent">
+          <p className="context">File Duplication</p>
+        </div>
+        <div className="servicecontent">
+          <p className="context">Digitization/Upscaling</p>
+        </div>
+        <div className="servicecontent">
+          <p className="context">UX/UI Design</p>
+        </div>
+        <div className="servicecontent">
+          <p className="context">Web/Software Development</p>
+        </div>
+      </section>
     </main>
-  )
-}
+  );
+};
 
-export default ServicesContent
+export default ServicesContent;
