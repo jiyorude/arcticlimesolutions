5827b0dded9204540f08e5982dc58bb74078a43a
Jordy Veenstra <jordy.gaptx@gmail.com>
2023-07-20 20:39:04 +0200

    20/07 20:38
    Grid complete, scss adjustments, working on hover animation where selected text changes color randomly to one of set colors

--- arcticlimesolutions/src/components/ServicesContent.js
+++ arcticlimesolutions/src/components/ServicesContent.js
@@ -1,14 +1,44 @@
 import React from "react";
+import { useState } from "react";
 import "../css/services.min.css";
 import Editing from '../image/jpg/services/Editing.jpg';
 import MoGraph from "../image/jpg/services/Mograph.jpg";
 import Audio from "../image/jpg/services/Audio.jpg";
 import Animation from "../image/jpg/services/Animatie.jpg";
 import Subtitling from "../image/jpg/services/Subtitling.jpg";
+import Delivery from "../image/jpg/services/Delivery.jpg";
+import Authoring from "../image/jpg/services/Authoring.jpg";
+import Duplication from "../image/jpg/services/Duplicatie.jpg";
+import UXUI from "../image/jpg/services/UX.jpg";
+import WebDev from "../image/jpg/services/Web.jpg";
+import Upscale from "../image/jpg/services/Upscale.jpg";
+import Translation from "../image/jpg/services/Translation.jpg";
+
+const colorDB = {
+  colors: [
+    "#D0FF14",
+    "#BA1A50",
+    "#32A0AE",
+    "#32B156",
+    "#9DE637",
+  ]
+};
 
 const ServicesContent = () => {
+
+  const [coloring, setColoring] = useState(colorDB.colors[0]);
+
+  const handleColor = () => {
+    const randomColor = Math.floor(Math.random()  * colorDB.colors.length);
+    setColoring(colorDB.colors[randomColor]);
+  };
+
+  const returnColor = {
+    color: "#FAFAFA"
+  };
+  
   return (
-    <main className="flowroot">
+    <main className="flowroot white">
       <section className="servicegrid">
         <div
           className="servicecontent editing"
@@ -16,7 +46,14 @@ const ServicesContent = () => {
             backgroundImage: `url(${Editing})`,
           }}
         >
-          <p className="context">Video Editing</p>
+          <p
+            className="context"
+            style={{ color: coloring ? coloring : returnColor.color }}
+            onMouseEnter={handleColor}
+            onMouseLeave={() => setColoring("")}
+          >
+            Video Editing
+          </p>
         </div>
         <div
           className="servicecontent mograph"
@@ -24,7 +61,14 @@ const ServicesContent = () => {
             backgroundImage: `url(${MoGraph})`,
           }}
         >
-          <p className="context">Motion Graphics</p>
+          <p
+            className="context"
+            style={{ color: coloring ? coloring : returnColor.color }}
+            onMouseEnter={handleColor}
+            onMouseLeave={() => setColoring("")}
+          >
+            Motion Graphics
+          </p>
         </div>
         <div
           className="servicecontent audio"
@@ -32,7 +76,14 @@ const ServicesContent = () => {
             backgroundImage: `url(${Audio})`,
           }}
         >
-          <p className="context">Audio Post</p>
+          <p
+            className="context"
+            style={{ color: coloring ? coloring : returnColor.color }}
+            onMouseEnter={handleColor}
+            onMouseLeave={() => setColoring("")}
+          >
+            Audio Post
+          </p>
         </div>
         <div
           className="servicecontent animation"
@@ -40,7 +91,14 @@ const ServicesContent = () => {
             backgroundImage: `url(${Animation})`,
           }}
         >
-          <p className="context">Animation</p>
+          <p
+            className="context"
+            style={{ color: coloring ? coloring : returnColor.color }}
+            onMouseEnter={handleColor}
+            onMouseLeave={() => setColoring("")}
+          >
+            Animation
+          </p>
         </div>
         <div
           className="servicecontent subs"
@@ -48,28 +106,113 @@ const ServicesContent = () => {
             backgroundImage: `url(${Subtitling})`,
           }}
         >
-          <p className="context">Subtitling</p>
+          <p
+            className="context"
+            style={{ color: coloring ? coloring : returnColor.color }}
+            onMouseEnter={handleColor}
+            onMouseLeave={() => setColoring("")}
+          >
+            Subtitling
+          </p>
         </div>
-        <div className="servicecontent">
-          <p className="context">Translation</p>
+        <div
+          className="servicecontent translation"
+          style={{
+            backgroundImage: `url(${Translation})`,
+          }}
+        >
+          <p
+            className="context"
+            style={{ color: coloring ? coloring : returnColor.color }}
+            onMouseEnter={handleColor}
+            onMouseLeave={() => setColoring("")}
+          >
+            Translation
+          </p>
         </div>
-        <div className="servicecontent">
-          <p className="context">Delivery</p>
+
+        <div
+          className="servicecontent delivery"
+          style={{
+            backgroundImage: `url(${Delivery})`,
+          }}
+        >
+          <p
+            className="context"
+            style={{ color: coloring ? coloring : returnColor.color }}
+            onMouseEnter={handleColor}
+            onMouseLeave={() => setColoring("")}
+          >
+            Delivery
+          </p>
         </div>
-        <div className="servicecontent">
-          <p className="context">DVD/Blu-Ray Authoring</p>
+        <div
+          className="servicecontent authoring"
+          style={{
+            backgroundImage: `url(${Authoring})`,
+          }}
+        >
+          <p
+            className="context"
+            style={{ color: coloring ? coloring : returnColor.color }}
+            onMouseEnter={handleColor}
+            onMouseLeave={() => setColoring("")}
+          >
+            DVD/Blu-Ray Authoring
+          </p>
         </div>
-        <div className="servicecontent">
-          <p className="context">File Duplication</p>
+        <div
+          className="servicecontent duplication"
+          style={{
+            backgroundImage: `url(${Duplication})`,
+          }}
+        >
+          <p
+            className="context"
+            style={{ color: coloring ? coloring : returnColor.color }}
+            onMouseEnter={handleColor}
+            onMouseLeave={() => setColoring("")}
+          >
+            File Duplication
+          </p>
         </div>
-        <div className="servicecontent">
-          <p className="context">Digitization/Upscaling</p>
+        <div
+          className="servicecontent upscale"
+          style={{
+            backgroundImage: `url(${Upscale})`,
+          }}
+        >
+          <p
+            className="context"
+            style={{ color: coloring ? coloring : returnColor.color }}
+            onMouseEnter={handleColor}
+            onMouseLeave={() => setColoring("")}
+          >
+            Digitization/Upscaling
+          </p>
         </div>
-        <div className="servicecontent">
-          <p className="context">UX/UI Design</p>
+        <div
+          className="servicecontent uxui"
+          style={{
+            backgroundImage: `url(${UXUI})`,
+          }}
+        >
+          <p
+            className="context"
+            style={{ color: coloring ? coloring : returnColor.color }}
+            onMouseEnter={handleColor}
+            onMouseLeave={() => setColoring("")}
+          >
+            UX/UI Design
+          </p>
         </div>
-        <div className="servicecontent">
-          <p className="context">Web/Software Development</p>
+        <div
+          className="servicecontent webdev"
+          style={{
+            backgroundImage: `url(${WebDev})`,
+          }}
+        >
+          <p className="context" style={{color: coloring ? coloring : returnColor.color}} onMouseEnter={handleColor} onMouseLeave={() => setColoring("")}>Web/Software Development</p>
         </div>
       </section>
     </main>
