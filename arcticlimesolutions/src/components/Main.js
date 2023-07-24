f9b2d6e4663fb2f97c45fd8bde08c090bd588b13
Jordy Veenstra <jordy.gaptx@gmail.com>
2023-07-24 15:58:41 +0200

    24/07 15:58
    Tried implementing react page transitions but did not work yet

--- arcticlimesolutions/src/components/Main.js
+++ arcticlimesolutions/src/components/Main.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from "react";
 import "../css/home.min.css";
-import indexvid from '../video/index.mp4';
-import { useState } from 'react';
+import "../css/up.min.css";
+import indexvid from "../video/index.mp4";
+import { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
+
 
 const Main = () => {
   const [service, setService] = useState(false);
@@ -12,13 +14,13 @@ const Main = () => {
   const styleService = {
     color: service ? "#BA1A50" : "#D0FF14",
     marginTop: service ? "-2px" : "0",
-    transition: "margin-top 0.3s ease"
+    transition: "margin-top 0.3s ease",
   };
 
   const styleProject = {
     color: project ? "#BA1A50" : "#D0FF14",
     marginTop: project ? "-2px" : "0",
-    transition: "margin-top 0.3s ease"
+    transition: "margin-top 0.3s ease",
   };
 
   const styleRate = {
@@ -38,44 +40,44 @@ const Main = () => {
   const handleProject = () => {
     setProject(!project);
   };
-  
+
   return (
     <>
       <main className="container">
-        <video loading="lazy" autoPlay loop muted className='fullscreen'>
+        <video loading="lazy" autoPlay loop muted className="fullscreen">
           <source src={indexvid} type="video/mp4" />
           Your video does not support fullscreen video.
         </video>
         <h1>Solutions. For every screen.</h1>
-        </main>
-        <section className="menu">
-          <ul className='unordered'>
-            <li>
-              <Link to={"/services"} style={{ textDecoration: "none" }}>
-                <h6 style={styleService} onMouseEnter={handleService} onMouseLeave={handleService}>
-                  SERVICES
-                </h6>
-              </Link>
-            </li>
-            <li>
-              <Link to={"/work"} style={{ textDecoration: "none" }}>
-                <h6 style={styleProject} onMouseEnter={handleProject} onMouseLeave={handleProject}>
-                  PROJECTS
-                </h6>
-              </Link>
-            </li>
-            <li>
-              <Link to={"/rates"} style={{ textDecoration: "none" }}>
-                <h6 style={styleRate} onMouseEnter={handleRate} onMouseLeave={handleRate}>
-                  RATES
-                </h6>
-              </Link>
-            </li>
-          </ul>
-        </section>
+      </main>
+      <section className="menu">
+        <ul className="unordered">
+          <li>
+            <Link to={"/services"} style={{ textDecoration: "none" }}>
+              <h6 style={styleService} onMouseEnter={handleService} onMouseLeave={handleService}>
+                SERVICES
+              </h6>
+            </Link>
+          </li>
+          <li>
+            <Link to={"/work"} style={{ textDecoration: "none" }}>
+              <h6 style={styleProject} onMouseEnter={handleProject} onMouseLeave={handleProject}>
+                PROJECTS
+              </h6>
+            </Link>
+          </li>
+          <li>
+            <Link to={"/rates"} style={{ textDecoration: "none" }}>
+              <h6 style={styleRate} onMouseEnter={handleRate} onMouseLeave={handleRate}>
+                RATES
+              </h6>
+            </Link>
+          </li>
+        </ul>
+      </section>
       <Outlet />
     </>
   );
 };
 
-export default Main
+export default Main;
