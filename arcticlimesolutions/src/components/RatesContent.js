2e3f1909069ad2684bec8c7a0dfb88db49f51eb2
Jordy Veenstra <jordy.gaptx@gmail.com>
2023-07-31 17:33:53 +0200

    31/07 17:33
    Rates: Creating content construction for rates page

--- arcticlimesolutions/src/components/RatesContent.js
+++ arcticlimesolutions/src/components/RatesContent.js
@@ -4,81 +4,25 @@ import { motion } from "framer-motion";
 
 const RatesContent = () => {
   return (
-    <motion.main className="flowroot" 
-    initial={{ opacity: 0 }} 
-    animate={{ opacity: 1 }} 
-    exit={{ opacity: 0 }}>
+    <motion.main className="flowroot" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+      <section className="row">RATES AND PRICES</section>
+      <section className="tax">
+        Alle prijzen zijn, waar BTW van toepassing is, exclusief 21% BTW.<br></br>All prices are, where VAT is
+        appliccable, excluding 21% VAT.
+      </section>
       <main className="pricescontainer">
-        <div className="serviceOne">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti distinctio saepe architecto hic rem, minima
-          sint. Nisi odit tempore aperiam hic. Molestiae, expedita non sapiente culpa tempore temporibus accusantium
-          totam! Deserunt illo atque velit consectetur. Voluptate magni commodi repellat corrupti unde dolor eligendi
-          sed animi quas? Illo sequi sit sed deserunt, amet vero voluptas unde culpa quo, alias dignissimos
-          voluptatibus? Nobis ut ea alias dignissimos aspernatur unde sunt aperiam sed eveniet, ipsa, enim facilis
-          repellendus quos maiores provident! Cupiditate quod ex tempora provident quae dolorem tempore libero vitae
-          repellendus ad. Deserunt at harum vero, quas et sapiente. Odit quae odio eius culpa repellat recusandae
-          blanditiis pariatur laboriosam aut adipisci sed tempora placeat reiciendis rerum, illum facilis nulla ipsum
-          saepe dignissimos. Ratione quasi nobis expedita totam recusandae qui eius ullam eligendi eaque aut! Nam non
-          praesentium, sint mollitia ipsam enim nisi modi quod! Beatae iure, eum illo quaerat inventore eligendi
-          similique? Obcaecati odio at voluptatum suscipit, exercitationem aut aliquid facilis deserunt neque laboriosam
-          commodi odit ipsa cumque est modi omnis doloribus ipsum! Quidem ipsam repellat impedit magnam labore at,
-          cumque vero. Optio, aspernatur ullam. Neque error quasi vel molestias debitis cum natus unde nulla laborum
-          iusto dignissimos odio assumenda minima incidunt quidem ad doloremque quos, quae porro libero voluptas
-          nesciunt id. Quisquam, maiores sit vitae quae ducimus placeat, voluptatibus impedit, suscipit eum iste modi
-          excepturi aperiam aspernatur. Commodi maxime tenetur, repellat accusamus eaque molestias minus at molestiae
-          natus cum enim praesentium. Quas facilis repudiandae quae consectetur tempore tenetur? Laboriosam facilis
-          ipsam impedit molestiae dignissimos asperiores voluptatem corporis adipisci amet? Cum doloremque expedita
-          placeat numquam est fuga animi magnam mollitia nam iusto. Delectus ullam nam accusantium ipsam nihil molestias
-          soluta perferendis. Velit, sequi eaque? Libero error minus nostrum, commodi voluptas facilis sequi suscipit
-          explicabo, rem, nisi tempora autem. Debitis voluptate magnam impedit!
-        </div>
-        <div className="priceOne">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam placeat accusamus aut, porro enim modi corrupti
-          sapiente reprehenderit nobis qui? In voluptatem nisi exercitationem architecto molestias dolorum impedit!
-          Nemo, iure. Saepe nostrum asperiores et placeat consectetur hic eum beatae maxime corrupti voluptates possimus
-          autem ex sed, ratione eius natus excepturi culpa quasi sint iusto inventore at similique dolore quia? Sed.
-          Modi, temporibus eaque vitae porro cum ad sunt nihil nam minus dolorem accusantium iste reiciendis iusto odio
-          optio itaque veniam? Nulla, omnis! Maxime temporibus similique laudantium non totam obcaecati blanditiis.
-          Praesentium officiis, nostrum facere quas corporis eaque quos fugit ea magnam est nam similique tempora
-          tempore odit, culpa aspernatur placeat omnis, minus rem nisi distinctio. Labore exercitationem rem eveniet
-          repellat!
-        </div>
-
-        <div className="serviceTwo">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus pariatur officiis officia dolor neque
-          perferendis suscipit, quidem maiores, distinctio, eveniet ratione temporibus ullam numquam sunt rem explicabo
-          aut? Aspernatur, minima? Sequi eligendi, quidem perspiciatis odio eum eos rem repellendus vel in autem error
-          excepturi expedita repellat. Tempora aspernatur omnis, sit quidem, eveniet ut accusantium molestiae voluptates
-          quisquam modi asperiores. Rerum. Suscipit eius impedit beatae earum nisi dicta veniam. Distinctio, impedit at
-          magnam quibusdam, ut aperiam animi recusandae veritatis temporibus facere quaerat maxime? Distinctio
-          consectetur architecto soluta ut exercitationem cumque quisquam. Doloribus voluptatibus adipisci laborum nulla
-          vero quo, ea cupiditate officia tempore, culpa commodi, delectus reprehenderit labore autem maiores illum
-          exercitationem aut aliquam. Nostrum minima repudiandae, vitae dolor laudantium cumque dolorem. Culpa, eius
-          blanditiis, tenetur a est debitis beatae nihil ducimus porro, pariatur quam in provident voluptate saepe.
-          Voluptate accusantium blanditiis nulla, quasi laborum illo ex magnam quo maxime. Ex, perspiciatis. Facilis,
-          qui. Quo excepturi doloremque harum quos vero cumque culpa minima nihil reprehenderit odit voluptatum, placeat
-          aperiam illum magnam voluptatem numquam libero quae pariatur distinctio mollitia facere reiciendis blanditiis
-          eveniet. Dolorem odio quaerat quae. Quis, hic! Qui inventore quae, alias a, molestias, reprehenderit id
-          molestiae doloremque corrupti mollitia error nam cumque quibusdam labore debitis vero quia. Voluptas fugit
-          fugiat excepturi? Amet placeat aliquid aliquam perspiciatis consequatur commodi cumque facere eos repudiandae?
-          Deserunt excepturi nisi voluptatibus suscipit consectetur? Quibusdam similique fugiat, ducimus est cumque
-          mollitia numquam vero itaque ad, aliquam ullam. Fugit, iusto quisquam magnam eum rerum in facilis maiores ab
-          pariatur cum consequatur, temporibus unde. Iusto quasi quidem quis magni natus corporis nesciunt, veritatis
-          ratione modi, repudiandae iste accusamus sint! Possimus ad at laudantium cupiditate molestiae facilis sunt
-          accusantium dicta quia labore, odio architecto neque. Itaque iure id sunt dolore, nulla deserunt explicabo
-          odio soluta nesciunt eum hic, illum ullam.
-        </div>
-        <div className="priceTwo">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam sequi, at doloremque cupiditate atque unde
-          aliquam iure illo rem expedita iste assumenda amet dolorem vitae. Nam impedit iure unde temporibus? Quibusdam,
-          error laudantium blanditiis sint sunt, explicabo iste assumenda inventore nam, magnam quaerat officiis
-          reprehenderit deleniti alias quos aliquid. Reprehenderit error, fugiat natus cumque quis commodi perspiciatis
-          itaque. Accusantium, quam? Exercitationem error, quae possimus nesciunt officia ipsa saepe, atque magnam
-          fugit, a minima. Veritatis, impedit animi vero amet error corrupti dolorum corporis, odio molestiae ducimus
-          voluptate fuga ipsa harum deleniti! Repudiandae, sequi omnis! Quasi, enim ipsam est similique aut eaque
-          voluptatibus itaque, non ducimus exercitationem veniam quae iste adipisci, animi dignissimos officiis neque
-          corrupti. Maxime obcaecati et dolorum nemo a.
-        </div>
+        <section className="serviceOne">
+          <div className="rateheader">Omschrijving</div>
+        </section>
+        <section className="priceOne">
+          <div className="rateheader">Prijs</div>
+        </section>
+        <section className="serviceTwo">
+          <div className="rateheader">Description</div>
+        </section>
+        <section className="priceTwo">
+          <div className="rateheader">Price</div>
+        </section>
       </main>
     </motion.main>
   );
