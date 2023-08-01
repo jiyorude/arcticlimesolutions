8fd7de8d4c5437cccbed293b6cea7bbc4d6a826d
Jordy Veenstra <jordy.gaptx@gmail.com>
2023-08-01 16:38:24 +0200

    01/08 16:39
    Updated scss for rates. Updated rates content

--- arcticlimesolutions/src/components/RatesContent.js
+++ arcticlimesolutions/src/components/RatesContent.js
@@ -13,15 +13,33 @@ const RatesContent = () => {
       <main className="pricescontainer">
         <section className="serviceOne">
           <div className="rateheader">Omschrijving</div>
+          <section className="whitespace"></section>
+          <div className="servdescr">Uurtarief</div>
+          <div className="servextra">
+            Dit uurtarief geldt voor alle standaard werkzaamheden en activiteiten, zoals: montage, programmeren,
+            audio-nabewerking, color-grading, visual effects en animatie.
+          </div>
+          <div className="servextra">
+            Dit uurtarief is inclusief een 'standaard delivery' van uw bestanden in bijvoorbeeld mp4, wav, mov of pdf.
+            Alle bestandsformaten die binnen het 'standaard delivery protocol' vallen zijn als volgt:{" "}
+          </div>
+          <div className="servextra bold">Video: MP4, MOV (APR4444XQ), GIF (1-2 seconden), JPG Sequentie</div>
+          <div className="servextra bold">Audio: WAV, MP3, AIFF in Web (-14LUFS) of TV (-23LUFS) Mix. Stereo</div>
         </section>
         <section className="priceOne">
-          <div className="rateheader">Prijs</div>
+          <div className="rateheader right">Prijs</div>
+          <section className="whitespace"></section>
+          <div className="servprice">€ 45,00</div>
         </section>
         <section className="serviceTwo">
           <div className="rateheader">Description</div>
+          <section className="whitespace"></section>
+          <div className="servdescr">Hourly Rate</div>
         </section>
         <section className="priceTwo">
-          <div className="rateheader">Price</div>
+          <div className="rateheader right">Price</div>
+          <section className="whitespace"></section>
+          <div className="servprice">€ 45,00</div>
         </section>
       </main>
     </motion.main>
