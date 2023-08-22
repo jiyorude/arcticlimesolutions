37e728ad8ddeed8feb24293992cb00bd085dee82
Jordy Veenstra <jordy.gaptx@gmail.com>
2023-08-23 00:45:19 +0200

    23/08 00:44
    Working on carrousel. Trying to fix an issue with project background images not loading

--- arcticlimesolutions/src/components/ProjectCarrousel.js
+++ arcticlimesolutions/src/components/ProjectCarrousel.js
@@ -1,29 +1,30 @@
-import React from 'react';
+import React from "react";
 import "../css/carrousel.min.css";
 import workData from "../database/work.json";
 
+
 const ProjectCarrousel = () => {
-    const ProjectBlock = ({project}) => {
-        const blockStyle = {
-            backgroundImage: `url(${project.thumbnail})`
-        }
-        return (
-            <div className="projectblock" style={blockStyle}>
-                <h3 className="projtitle">{project.title}</h3>
-                <p className="projdescr">{project.short}</p>
-            </div>
-        )
+  const ProjectBlock = ({ project }) => {
+    const blockStyle = {
+      backgroundImage: `url(${process.env.PUBLIC_URL + project.thumbnail})`,
     };
-  
     return (
+      <div className="projectblock" style={blockStyle}>
+        <h3 className="projtitle">{project.title}</h3>
+        <p className="projdescr">{project.short}</p>
+      </div>
+    );
+  };
+
+  return (
     <main className="carrouselgrid">
       <section className="gridcontent">
         {workData.map((project, index) => (
-            <ProjectBlock key={index} project={project} />
+          <ProjectBlock key={index} project={project} />
         ))}
       </section>
     </main>
   );
-}
+};
 
-export default ProjectCarrousel
+export default ProjectCarrousel;
