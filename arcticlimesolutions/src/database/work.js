0d2ecd80ae851b18b9648966ef5ece6f8334b217
Jordy Veenstra <jordy.gaptx@gmail.com>
2023-08-24 16:50:42 +0200

    24/08 16:50
    Updated work database, updated scss

--- arcticlimesolutions/src/database/work.js
+++ arcticlimesolutions/src/database/work.js
@@ -7,7 +7,7 @@ const projects = [
       "ARENA is a visual study about Quake III Arena; its environments and its textures; and a deeper critical look at over two-hundred maps that many of us seemed to have spent many hours in; to fight, frag, strafe, score and humiliate. The map as a 'being' is the outside layer of an onion, serving as the aesthetic layer for all of the nail-biting and nerve-wrecking last-moment capture the flag scores and all of the digital bloodshed that takes place within this vast universe where only the strongest can prevail.",
       "ARENA is antimatter; rejecting skill and traditional means of gameplay. No power-ups, weapons and/or players are found. It is you, your inner thoughts and the arena, inside a world that is empty, inside a universe that is limitless. The rest is up to you.",
     ],
-    client: "AR3NA",
+    client: "APPOV",
     roleOne: "Production",
     roleTwo: "Video-Editing",
     roleThree: "Grading and Audio",
@@ -16,16 +16,79 @@ const projects = [
   },
   {
     id: 2,
-    title: "",
-    short: "",
-    text: "",
-    client: "",
-    roleOne: "",
-    roleTwo: "",
-    roleThree: "",
-    thumbnail: "",
+    title: "Experience",
+    short: "Short Film",
+    text: [
+      "Experience is a multi-disciplinary, collaborative Machinima project produced by A Pixelated Point of View and game developer Pixelsplit in the form of a short, experimental film. Experience was produced in one of Pixelsplit's most popular games: Virtual Rides III; a funfair simulation game in which the player is given full control on the customization and control of more than twenty unique funfair rides on a fictional fairground. The final product consists of a visual study of the virtual fairground itself; aiming to highlight the premises, the machines and its visitors from as many different perspectives as possible.",
+      "The final product highlights the virtual funfair from a plethora of experimental camera point of views. The film focuses on the rides themselves, the visitors, the premises; all of them coming together in one, experimental piece.",
+      "Experience was used by the client as part of their marketing strategy to promote Virtual Rides III and uploaded to their YouTube channel, which received over two-thousand views.",
+    ],
+    client: "Pixelsplit",
+    roleOne: "Production",
+    roleTwo: "Video-Editing",
+    roleThree: "Grading and Audio",
+    thumbnail: require("../image/jpg/thumb/thumb_experience.jpg"),
     video: "",
   },
+  {
+    id: 3,
+    title: "MMF 2023",
+    short: "Poster Design",
+    text: [
+      "The Milan Machinima Festival is an annual film-festival held in Milano, Italy. The festival is a celebration of the world's best machinima and filmmakers who utilize video games and related technologies. This annual event features a diverse selection of game-based videos created by talented artists and practitioners whose work intersects video art, cinema, animation, and gaming. The showcased artworks are chosen by an international panel of jurors and a team of curators based on their cultural significance, artistic excellence, and innovative style.",
+      "ARCTIC LIME AudioVisual was chosen to design the poster for the 2023 edition of the festival. The poster was used for the marketing materials for the festival and used online and in print. The poster was generated using the Dall-E AI.",
+      "The initial keywords used to design the poster were 'npc' and 'ai'. In tandem with the festivals theme, initial thoughts were: pseudo-intelligence, digitization, dehumanization, empty shells, algorithms, compulsory movement and/or execution — as in NPC speech and movement —, the contrast between real/fabricated and the visualization of code.",
+      "The created work represents White experimental, dehumanized masses looking like NPCs, with truly absurd poses; just standing there, surrounded by experimental ‘white’ code blobs; simply looking around restlessly and waiting for someone, anyone, to give a quest to, to be useful and to have their code finally executed. To have their own quest completed.",
+    ],
+    client: "MMF",
+    roleOne: "",
+    roleTwo: "AI Operation",
+    roleThree: "Poster Design",
+    thumbnail: require("../image/jpg/thumb/thumb_mmf23.jpg"),
+    video: "",
+  },
+  {
+    id: 4,
+    title: "Dominion",
+    short: "JavaScript Algorithm",
+    text: [
+      "DOMINION is an algorithm written in JavaScript that generates random data for the production of experimental, depthmap-based Quake III Machinima. The algorithm outputs a list of various elements that correspond to Quake III Machinima-making processes and certain parameters found in Q3MME. Technically speaking, DOMINION is able to generate endless variations of potential experimental machinima films."
+    ],
+    client: "Dominion",
+    roleOne: "",
+    roleTwo: "",
+    roleThree: "Programming",
+    thumbnail: require("../image/jpg/thumb/thumb_dominion.jpg"),
+    video: "",
+  },
+  {
+    id: 5,
+    title: "Exotic",
+    short: "AI Art",
+    text: [
+      "Generated with BlueWillow AI. Edited in Adobe Photoshop"
+    ],
+    client: "Exotic",
+    roleOne: "",
+    roleTwo: "Photo Editing",
+    roleThree: "AI Operation",
+    thumbnail: require("../image/jpg/thumb/thumb_exotic.jpg"),
+    video: ""
+  },
+  {
+    id: 6,
+    title: "Chronometer",
+    short: "YouTube Content",
+    text: [
+      "Longform content for a YouTube Channel 'ChronometerCheck', which mainly focuses on watch reviews, best-of/top 10 video's, unboxings and other watch-related content"
+    ],
+    client: "Chronometer Check",
+    roleOne: "Audio Mix",
+    roleTwo: "Editing (YouTube/Social)",
+    roleThree: "Grading",
+    thumbnail: require("../image/jpg/thumb/thumb_chronometer.jpg"),
+    video: ""
+  }
 ];
 
 export default projects;
