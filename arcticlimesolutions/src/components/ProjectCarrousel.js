3bd107b4f0c956e260f4ad36494398bbe046b37a
Jordy Veenstra <jordy.gaptx@gmail.com>
2023-09-03 17:59:50 +0200

    03/09 18:00
    Added animations in project carrousel

--- arcticlimesolutions/src/components/ProjectCarrousel.js
+++ arcticlimesolutions/src/components/ProjectCarrousel.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "../css/carrousel.min.css";
 import projects from "../database/work";
+import { motion } from "framer-motion";
 
 const ProjectCarrousel = () => {
   const ProjectBlock = ({ project }) => {
     return (
-      <div className="projectblock" style={{ backgroundImage: `url(${project.thumbnail})` }}>
+      <motion.div
+        className="projectblock"
+        style={{ backgroundImage: `url(${project.thumbnail})` }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
         <h3 className="projtitle">{project.title}</h3>
         <p className="projdescr">{project.short}</p>
-      </div>
+      </motion.div>
     );
   };
 
