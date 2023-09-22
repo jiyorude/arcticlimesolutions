802bed32e01a926daf1d72ec9ce4cd8af18d9e67
Jordy Veenstra <jordy.gaptx@gmail.com>
2023-09-22 16:24:12 +0200

    22/09 16:23
    Adjusted manifest, rebuilt wip build, recommit to gh pages

--- arcticlimesolutions/src/index.js
+++ arcticlimesolutions/src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./css/main.min.css";
 import AnimatedRoutes from "./AnimatedRoutes";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter basename="/arcticlimesolutions">
       <AnimatedRoutes />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
